06ebb4b272efbf94845f9b2be94ebead8f361b94
Ryan Davis <ryandavis@deloitte.com>
2024-03-05 12:54:21 -0800

    Initial Commit

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,9 +1,9 @@
-import sys
+"""This module contains the calculator functions for the formulas square, tri, lazy caterer, and magic squares"""
 
 
 def squareNums(n):
     """Calculates the square"""
-    return n ** 2
+    return n**2
 
 
 def triNums(n):
@@ -13,21 +13,31 @@ def triNums(n):
 
 def lazyCaterer(n):
     """Calculates the lazy caterer number"""
-    return (n ** 2 + n + 2) / 2
+    return (n**2 + n + 2) / 2
 
 
 def magicSquares(n):
     """Calculates the magic squares number"""
-    return (n * (n ** 2 + 1)) / 2
+    return (n * (n**2 + 1)) / 2
+
+
+def run_calculator(input_formula, input_num):
+    """Calls and returns results for the specified formulas"""
+    calculator = [squareNums, triNums, lazyCaterer, magicSquares]
+    formula = calculator[input_formula - 1]
+    return_result = formula(input_num)
+    return return_result
 
 
 if __name__ == "__main__":
-    calculator = {
-        "square": squareNums,
-        "tri": triNums,
-        "lazy_caterer": lazyCaterer,
-        "magic_squares": magicSquares,
-    }
-    input_formula = calculator[sys.argv[1]]
-    result = input_formula(int(sys.argv[2]))
+    print(
+        "Choose which formula to use: 1 for square, 2 for tri, 3 for lazy caterer, 4 for magic squares"
+    )
+    input_formula = int(input())
+    print(
+        "Enter a number to calculate the square, tri, lazy caterer, and magic squares numbers"
+    )
+    input_num = int(input())
+
+    result = run_calculator(input_formula, input_num)
     print(result)
